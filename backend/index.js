1f359e0fc2c96202842ca84f3cafa112ea720b9e
Om-Sureja <omsureja@gmail.com>
2024-02-02 02:01:11 +0530

    added account routes with function such as get balanace and transfering money

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const database = require("./config/database");
 const userRoutes = require("./routes/user");
+const accountRoutes = require("./routes/account");
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -12,6 +13,7 @@ dotenv.config();
 database.connect();
 
 app.use("/user", userRoutes);
+app.use("/account", accountRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ backend/routes/account.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const { userMiddleware } = require("../middlewares/User");
+const { Account } = require("../db");
+const { default: mongoose } = require("mongoose");
+const router = express.Router();
+
+router.get("/balance", userMiddleware, async (req, res) => {
+  try {
+    const userAccount = await Account.findOne({
+      userId: req.user.id,
+    });
+    return res.status(200).json({
+      balance: userAccount.balance,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Server Error , Failed to get the balance",
+    });
+  }
+});
+
+router.post("/transfer", userMiddleware, async (req, res) => {
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const to = req.body.to;
+    const amount = req.body.amount;
+    if (!to || !amount) {
+      return res.status(401).json({
+        success: false,
+        msg: "Please provide all fields!",
+      });
+    }
+    const fromAccount = await Account.findOne({
+      userId: req.user.id,
+    }).session(session);
+
+    if (!fromAccount || fromAccount.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        msg: "Insufficient Balance",
+      });
+    }
+
+    const toAccount = await Account.findOne({
+      userId: to,
+    }).session(session);
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        msg: "Invalid Account",
+      });
+    }
+
+    await Account.updateOne(
+      {
+        userId: req.user.id,
+      },
+      {
+        $inc: {
+          balance: -amount,
+        },
+      }
+    ).session(session);
+
+    await Account.updateOne(
+      {
+        userId: to,
+      },
+      {
+        $inc: {
+          balance: amount,
+        },
+      }
+    ).session(session);
+
+    await session.commitTransaction();
+    return res.status(200).json({
+      success: true,
+      msg: "Transfer Successfull",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Server Error , Failed to transfer the money",
+    });
+  }
+});
+
+module.exports = router;
