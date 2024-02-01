fe2fc273f0271b3009ce730749dd30adb2c83277
Om-Sureja <omsureja@gmail.com>
2024-02-02 00:36:07 +0530

    created bank schema

--- backend/db.js
+++ backend/db.js
@@ -31,9 +31,24 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+//create a schema for bank details
+const bankSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId || String,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 // Create a model from the schema
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", bankSchema);
 
 module.exports = {
   User,
+  Account,
 };
